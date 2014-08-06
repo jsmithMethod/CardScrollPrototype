a25317f9e8a8d4e6bee16a078770b0bb228c714d
Jason Smith <jsmith@Jasons-Laptop.local>
2014-08-06 09:58:48 -0400

    Initial Commit

---
+++ CardScroll/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardScroll
+//
+//  Created by Jason Smith on 8/6/14.
+//  Copyright (c) 2014 Method. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CardScroll/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardScroll
+//
+//  Created by Jason Smith on 8/6/14.
+//  Copyright (c) 2014 Method. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
